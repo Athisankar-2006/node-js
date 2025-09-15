e6edecdd28721cb9b211798c61559b7022abf02e
Athisankar-2006 <athisankar2k6@gmail.com>
2025-09-15 22:08:04 +0530

    req and res added

--- server.js
+++ server.js
@@ -2,7 +2,18 @@ const http= require('http')
 
 
 const server= http.createServer((req,res)=>{
-    console.log("request made")
+    console.log("request made");
+    console.log(req.url);
+    console.log(req.method)
+
+
+    res.setHeader('Content-type','text/html')
+    res.write("<h1>haii</h1>")
+    res.write("<h1>do it anything for you</h1>")
+    res.end()
+
+  
+
 })
 
 server.listen(3000, 'localhost',()=>{
