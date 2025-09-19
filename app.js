99afc7cb13bad06a6a522e277068f65241ab50cf
Athisankar-2006 <athisankar2k6@gmail.com>
2025-09-19 20:05:24 +0530

    task 10 completed in express.js

---
+++ app.js
@@ -0,0 +1,25 @@
+const exp= require('express')
+const app =exp()
+
+
+app.listen(3000);
+app.get('/',(req,res)=>{
+    res.sendFile('./docs/index.html',{root: __dirname})
+})
+
+app.get('/join',(req,res)=>{
+    res.sendFile('./docs/join.html',{root: __dirname})
+})
+
+
+app.get('/about',(req,res)=>{
+    res.sendFile('./docs/about.html',{root: __dirname})
+})
+
+app.get('/joinus',(req,res)=>{
+    res.redirect('/join') 
+})
+
+app.use((req,res)=>{
+    res.sendFile('./docs/notFound.html',{root: __dirname})
+})
