c1f3c38dc42e21f2eaa881e6f8ba1389bf7fcc32
Athisankar-2006 <athisankar2k6@gmail.com>
2025-09-19 20:40:07 +0530

    task 11 -middleware added

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
 const exp= require('express')
+const morgan =require('morgan')
 const app =exp()
 
 
 app.listen(3000);
+
+app.use(morgan('dev'))
+
+app.use((req,res,next)=>{
+    console.log('middle ware')
+    // console.log(req.host);
+    // console.log(req.path);
+    next();
+})
+
+
+app.use((req,res,next)=>{
+    console.log('middle ware 2');
+    next();
+})
+
+
 app.get('/',(req,res)=>{
     res.sendFile('./docs/index.html',{root: __dirname})
 })
@@ -21,5 +39,8 @@ app.get('/joinus',(req,res)=>{
 })
 
 app.use((req,res)=>{
-    res.sendFile('./docs/notFound.html',{root: __dirname})
+    res.status(404).sendFile('./docs/notFound.html',{root: __dirname})
 })
+
+
+//browser -> request ->server(middle ware) -> reponse ->browser
